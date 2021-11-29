446c762ffed15106e75be847dda5e5b3f2f516f2
Alex Alexeyev <aasmc@yandex.ru>
2021-11-29 22:04:26 +0300

    Add router for profile screens

--- app/src/main/java/ru/aasmc/jetreddit/routing/MyProfileRouter.kt
+++ app/src/main/java/ru/aasmc/jetreddit/routing/MyProfileRouter.kt
@@ -1,2 +1,17 @@
 package ru.aasmc.jetreddit.routing
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+sealed class MyProfileScreenType {
+    object Posts : MyProfileScreenType()
+    object About : MyProfileScreenType()
+}
+
+object MyProfileRouter {
+    var currentScreen: MutableState<MyProfileScreenType> = mutableStateOf(MyProfileScreenType.Posts)
+
+    fun navigateTo(destination: MyProfileScreenType) {
+        currentScreen.value = destination
+    }
+}
