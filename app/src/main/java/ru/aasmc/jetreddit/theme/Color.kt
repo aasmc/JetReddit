48035af8ec9aa916cccb95199ffd4150cde2a2a9
Alex Alexeyev <aasmc@yandex.ru>
2021-11-29 21:55:37 +0300

    Add basic theme

--- app/src/main/java/ru/aasmc/jetreddit/theme/Color.kt
+++ app/src/main/java/ru/aasmc/jetreddit/theme/Color.kt
@@ -1,2 +1,9 @@
 package ru.aasmc.jetreddit.theme
 
+import androidx.compose.ui.graphics.Color
+
+val RwPrimary = Color.White
+val RwPrimaryDark = Color.Black
+val RwAccent = Color(0xFF3F51B5)
+val LightBlue = Color(0xFF2196F3)
+val Red800 = Color(0xFFD00036)
