9b8e93af5dfe12a6d3ec4f224f3e574f97a84d79
Alex Alexeyev <aasmc@yandex.ru>
2021-11-29 22:02:38 +0300

    Add router across app screens

--- app/src/main/java/ru/aasmc/jetreddit/routing/JetRedditRouter.kt
+++ app/src/main/java/ru/aasmc/jetreddit/routing/JetRedditRouter.kt
@@ -1,2 +1,59 @@
 package ru.aasmc.jetreddit.routing
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import ru.aasmc.jetreddit.R
+
+sealed class Screen(
+    val titleResId: Int
+) {
+    object Home : Screen(R.string.home)
+    object Subscriptions : Screen(R.string.subreddits)
+    object NewPost : Screen(R.string.new_post)
+    object MyProfile : Screen(R.string.my_profile)
+}
+
+object JetRedditRouter {
+
+    var currentScreen: MutableState<Screen> = mutableStateOf(Screen.Home)
+
+    private var previousScreen: MutableState<Screen> = mutableStateOf(Screen.Home)
+
+    fun navigateTo(destination: Screen) {
+        previousScreen.value = currentScreen.value
+        currentScreen.value = destination
+    }
+
+    fun goBack() {
+        currentScreen.value = previousScreen.value
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
